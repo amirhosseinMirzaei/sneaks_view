3113b16b75d171a84cdc547ca96aa9fee4642e9a
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-05-27 23:49:21 -0700

    details

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -32,8 +32,55 @@ class DetailScreen extends StatelessWidget {
               ),
             ],
           ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text('Jurdan #251', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white)),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text('Owned by amir', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white70)),
+          ),
+          SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [_InfoTile(title: "3d 5h 23m", subtitle: "remaining time"), _InfoTile(title: "16.8 ETH", subtitle: "Highest Bid")],
+            ),
+          ),
+          const Spacer(),
+          Container(
+            width: double.infinity,
+            height: 40,
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Color(0xff3000ff)),
+            child: Text('Place Bid', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
+          ),
+          SizedBox(height: 60),
         ],
       ),
+    );
+  }
+}
+
+class _InfoTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const _InfoTile({super.key, required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
+        const SizedBox(height: 5),
+        Text(subtitle, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white70)),
+      ],
     );
   }
 }
